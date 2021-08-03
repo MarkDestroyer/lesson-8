2f3f63c43be02cb0393a565fa13e66b31db32021
MarkDestroyer <markkir123@gmail.com>
2021-08-03 22:44:09 +0300

    swift
    make homework for lesson 8.

---
+++ lesson-8 /client-server-1347/vk-api/Controllers/NewsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewsViewController.swift
+//  client-server-1347
+//
+//  Created by Марк Киричко on 03.08.2021.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
